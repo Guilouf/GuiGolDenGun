e3528e958cab54d5fe75921e02803eb6360db6e1
OSofiane <omari.sofiane@yahoo.fr>
2015-10-19 21:21:50 +0200

    yep

--- Supprimer_lignes_vides.py
+++ Supprimer_lignes_vides.py
@@ -11,15 +11,15 @@ def Supprimer_Interligne(path):
         mesFichiers = glob.glob(dossier+"\\*")
         for fichier in mesFichiers:
             chaine=fichier.split(os.sep)
-            pattern0=chaine[-2]
-            print pattern0[-2:]
-            pattern1=os.path.basename(fichier)
-##            monFichier = file(fichier)
-##            output_file=open(path+"\\"+pattern0+'_'+pattern1+"_Traite.txt",'w')
-##            for line in monFichier.readlines():
-##                if line.rstrip():
-##                    output_file.write(line)
-##    output_file.close()    
-##    
+            p=chaine[-2]
+            pattern0 =p[-2:]
+            pattern1 =os.path.basename(fichier)
+            monFichier = file(fichier)
+            output_file=open(path+"\\"+pattern0+'_'+pattern1+"_Traite.txt",'w')
+            for line in monFichier.readlines():
+                if line.rstrip():
+                    output_file.write(line)
+    output_file.close()    
+    
 Supprimer_Interligne("C:\\Users\\Sofiane\\Desktop\\M2_BIG\\ADT\\Projet_2015\\Donnees\\archives_SFBI\\archives_SFBI\\2015_06_10-bioinfo_archives_annee_2014")
 

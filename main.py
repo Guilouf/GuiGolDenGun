24d5038bfe9a1943103030d0622d31e6fc8a6d4f
Guilouf <Guilouf@users.noreply.github.com>
2015-12-03 15:51:00 +0100

    [guillaume] [14h] [amélioration du main]

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from parsDemail import Parsemail
 from FichierInverse import FichierIverse
 from tfidf import TfIdf
 from recherche import Recherche
+import os
+import psutil
 
 """
 #################
@@ -13,26 +15,44 @@ filepath = "C:/Users/Guigui/Desktop/M2/ADT/Moteur_Recherche/archives_SFBI/2015_0
 pathFR = "C:/Users/Guigui/Desktop/M2/ADT/Moteur_Recherche/Outils/common_words.total_fr.txt"        
 pathENG = "C:/Users/Guigui/Desktop/M2/ADT/Moteur_Recherche/Outils/common_words.total_en.txt"
 
+def initialisation():
+    pm = Parsemail(filepath,pathFR,pathENG)#tain c quoi déja le truc pour écraser les méthodes??la surcharge, des constructeurs
+    listeDmail, listeDmailRaci = pm.parsemail()
+    
+    
+    
+    fi = FichierIverse()
+    dicoInv, nbmotsdocs, nbmotCorpus =fi.fichInv(listeDmail)
+    dicoInvR, nbmotsdocsR, nbmotCorpusR = fi.fichInv(listeDmailRaci)
+    
+    
+    ti = TfIdf(False,dicoInv,nbmotsdocs,nbmotCorpus)
+    ti.calcul()
+    ti.serialisation()
+    ti2 =TfIdf(True, dicoInvR, nbmotsdocsR, nbmotCorpusR)#le true autorise la concaténation avec l'ancien dico
+    ti2.calcul()
+    ti2.serialisation()
 
-pm = Parsemail(filepath,pathFR,pathENG)#tain c quoi déja le truc pour écraser les méthodes??la surcharge, des constructeurs
-listeDmail, listeDmailRaci = pm.parsemail()
+initialisation()
 
-
-
-fi = FichierIverse()
-dicoInv, nbmotsdocs, nbmotCorpus =fi.fichInv(listeDmail)
-dicoInvR, nbmotsdocsR, nbmotCorpusR = fi.fichInv(listeDmailRaci)
-
-
-ti = TfIdf(False,dicoInv,nbmotsdocs,nbmotCorpus)
-ti.calcul()
-ti.serialisation()
-ti2 =TfIdf(True, dicoInvR, nbmotsdocsR, nbmotCorpusR)
-ti2.calcul()
-ti2.serialisation()
-
-rec = Recherche(True)
-dicoreq = rec.traitement_requete("tarragona") #tarragona spain genomes biology biologie
+rec = Recherche(True)#le truc indique que la rechecher va se porter aussi sur les mots racinisés.
+dicoreq = rec.traitement_requete("batard") #tarragona spain genomes biology biologie
     
 rech = rec.rech(dicoreq)
 print(dicoreq)
+
+
+process = psutil.Process(os.getpid())
+print(process.memory_info().rss / 1000000) #rss : resident set size
+print(process.cpu_times())
+
+""" c'est cool ca
+def __init__(self,arg1=None,arg2=None):
+    if arg1 and arg2 :
+        # traitement si argument 1 et 2
+        ...
+    else :
+        # traitement si pas d'argument
+       ...
+
+"""

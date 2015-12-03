85929d815e2526ea29a03cf7f95a987461d3fdd7
Guilouf <Guilouf@users.noreply.github.com>
2015-12-03 19:50:12 +0100

    [guillaume] [netoyage des fichiers]

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def initialisation():
 initialisation()#commenter pour faire des recherches successives
 
 rec = Recherche(True,1)#le true indique que la rechecher va se porter aussi sur les mots racinisés. le 1 veut dire mm poid racinisé et nn raci, 2= 2*- de poid pr les racinisés
-dicoreq = rec.traitement_requete("batard") #tarragona spain genomes biology biologie
+dicoreq = rec.traitement_requete("tarragona") #tarragona spain genomes biology biologie
     
 rech = rec.rech(dicoreq)
 print(dicoreq)
